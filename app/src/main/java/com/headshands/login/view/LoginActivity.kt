7e974cd21f7e5b962a26e02d97a6a9685c981625
vkowalsky <vkowalsky@gmail.com>
2019-02-26 00:03:50 +0300

    files

--- app/src/main/java/com/headshands/login/view/LoginActivity.kt
+++ app/src/main/java/com/headshands/login/view/LoginActivity.kt
@@ -27,6 +27,11 @@ import java.util.concurrent.TimeUnit
 import com.jakewharton.rxbinding2.view.RxView
 import android.R.attr.password
 import com.jakewharton.rxbinding2.widget.RxCompoundButton
+import android.R.attr.password
+import android.text.Editable
+import android.text.TextWatcher
+import com.headshands.R
+import com.jakewharton.rxbinding2.view.clickable
 
 
 class LoginActivity : AppCompatActivity() {
@@ -34,153 +39,22 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.headshands.R.layout.activity_login)
-        initialize()
+        initializeListeners()
     }
 
-    private fun initialize(){
+    private fun initializeListeners(){
+        editText_email.addTextChangedListener(Textwatcher(editText_email))
+        editText_password.addTextChangedListener(Textwatcher(editText_password))
 
-        val emailObservable = RxTextView.afterTextChangeEvents(editText_email)
-            .skipInitialValue()
-            .map {
-                wrapper_email.error = null
-                it.view().text.toString()
-            }
-            .debounce(1, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread())
-            .compose(verifyEmailPattern)
-            .compose(retryWhenError {
-                wrapper_email.error = it.message
-            })
-            //.subscribe()
-
-        val passwordObservable = RxTextView.afterTextChangeEvents(editText_password)
-            .skipInitialValue()
-            .map {
-                wrapper_password.error = null
-                it.view().text.toString()
-            }
-            .debounce(1, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread())
-            .compose(verifyPasswordLength)
-            .compose(retryWhenError {
-                wrapper_password.error = it.message
-            })
-            //.subscribe()
-
-        Observable.combineLatest(arrayOf(emailObservable, passwordObservable)) {
-            if(true) {
-                println("emailObservable --- "+emailObservable)
-                println("passwordObservable --- "+passwordObservable)
-            }
-        }.subscribe() {
-            println(it)
+        button_login.setOnClickListener {
+            showSnackBar(it)
         }
-  /*
-        val isSignInEnabled: Observable<Boolean> = Observable.combineLatest(
-            emailObservable,
-            passwordObservable,
-            BiFunction { u, p -> u == "true" && p == "true" })
-*/
-
-        fun test(args: Array<String>) {
-            val first = Observable.intervalRange(1, 10, 0, 2, TimeUnit.SECONDS)
-            val second = Observable.intervalRange(10, 20, 0, 1, TimeUnit.SECONDS)
-
-            Observable.combineLatest(arrayOf(first, second)) {
-                "${it[0]} -> ${it[1]}"
-            }.blockingSubscribe() {
-                println(it)
-            }
-        }
-//***************************************************************
-    /*    val itemInputNameObservable1 = RxTextView.afterTextChangeEvents(editText_email)
-            .skipInitialValue()
-            .map {
-                wrapper_email.error = null
-                it.view().text.toString()
-            }
-  */              /*
-                .skipInitialValue()
-            .map {
-                wrapper_email.error = null
-                it.view().text.toString()
-            }
-            .debounce(1, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread())
-            .compose(verifyEmailPattern)
-            .compose(retryWhenError {
-                wrapper_email.error = it.message
-            })
-            .subscribe()
-                 */
-/*
-        val itemInputNameObservable2 = RxTextView.afterTextChangeEvents(editText_email)
-            .skipInitialValue()
-            .map {
-                wrapper_email.error = null
-                it.view().text.toString()
-            }
-*/
-            /*RxTextView.textChanges(autocomplete_textView)
-                .map { inputText: CharSequence -> inputText.isEmpty()
-                        || !isValidCityInput(inputText.toString()) }
-                .distinctUntilChanged()*/
-/*
-        val isSignInEnabled: Observable<Boolean> = Observable.combineLatest(
-            itemInputNameObservable2,
-            itemInputNameObservable1,
-            BiFunction { u, p -> u == "true" && p == "true" })
-*/
-
+        button_login.setEnabled(false);
     }
 
-    private val verifyPasswordLength = ObservableTransformer<String, String> { observable ->
-        observable.flatMap {
-            Observable.just(it).map { it.trim() } //removing white spaces on the string
-                .filter { isValidPassword(it)} //setting the length to 8
-                .singleOrError()
-                .onErrorResumeNext {
-                    if (it is NoSuchElementException) {
-                        Single.error(Exception("Password must have at least 6 characters with at least one Capital letter, at least one lower case letter and at least one number")) //informing the user
-                    } else {
-                        Single.error(it)
-                    }
-                }
-                .toObservable()
-        }
-    }
+     //               Patterns.EMAIL_ADDRESS.matcher(it).matches() // setting the email pattern - abc@def.com -
 
-    private val verifyEmailPattern = ObservableTransformer<String, String> { observable ->
-        observable.flatMap {
-            Observable.just(it).map { it.trim() } //removing white spaces
-                .filter {
-                    Patterns.EMAIL_ADDRESS.matcher(it).matches() // setting the email pattern - abc@def.com -
-                }
-                .singleOrError()
-                .onErrorResumeNext {
-                    if (it is NoSuchElementException) {
-                        Single.error(Exception("Invalid email format")) //informing the usher
-                    } else {
-                        Single.error(it)
-                    }
 
-                }.toObservable()
-        }
-    }
-
-    private inline fun retryWhenError(crossinline onError: (ex: Throwable) -> Unit): ObservableTransformer<String, String> = ObservableTransformer { observable ->
-        observable.retryWhen { errors ->
-            errors.flatMap {
-                onError(it)
-                Observable.just("")
-            }
-        }
-    }
-
-    fun isValidPassword(password: String?) : Boolean {
-        password?.let {
-            val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{6,}$"
-            val passwordMatcher = Regex(passwordPattern)
-            return (passwordMatcher.find(password) != null)
-        } ?: return false
-    }
 
 
     /*
@@ -228,53 +102,75 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        fun showSnackBar(view: View){
-            //Snackbar(view)
-            val snackbar = Snackbar.make(view, "Replace with your own action",
-                Snackbar.LENGTH_LONG).setAction("Action", null)
-            snackbar.setActionTextColor(Color.BLUE)
-            val snackbarView = snackbar.view
-            snackbarView.setBackgroundColor(Color.LTGRAY)
-            val textView =
-                snackbarView.findViewById(android.support.design.R.id.snackbar_text) as TextView
-            textView.setTextColor(Color.BLUE)
-            textView.textSize = 28f
-            snackbar.show()
-        }
-  /*
-        fun showSnackBar(v: View) {
-            val snackbar = Snackbar
-                .make(v, "An Error Occurred!", Snackbar.LENGTH_LONG)
-                .setAction("RETRY") { }
-            snackbar.setActionTextColor(Color.BLUE)
-            val snackbarView = snackbar.view
-            val textView = snackbarView.findViewById(android.support.design.R.id.snackbar_text)
-            textView.setTextColor(Color.RED)
-            snackbar.show()
-        }
-*/
-        /*
-        CurrentWeather(coord=Coord(lon=-73.99, lat=40.73), weather=[Weather(id=502, main=Rain, description=heavy intensity rain, icon=10n), Weather(id=701, main=Mist, description=mist, icon=50n)], base=stations, main=Main(temp=3.42, tempMin=2.0, tempMax=6.0, pressure=1012.0, seaLevel=null, grndLevel=null, humidity=93, tempKf=null), wind=Wind(speed=4.06, deg=95.5032), clouds=Clouds(all=90), rain=Rain(h=null), dt=1551003300, sys=Sys(type=1, id=5141, message=0.005, country=US, sunrise=1551008224, sunset=1551048103, pod=null), id=5128581, name=New York, cod=200)
-         */
-       /*
-        val shopsService = ApiFactory.shopsApi
 
-        GlobalScope.launch(Dispatchers.Main) {
-            val shopListRequest = shopsService.getData()
-            try {
-                val response = shopListRequest.await()
-                if(response.isSuccessful){
-                    val shopsResponse = response.body() //This is single object Tmdb Movie response
-                    val list  = shopsResponse?.message // This is list of TMDB Movie
-                    Log.d("MainActivity ","jr")
-                }else{
-                    Log.d("MainActivity ",response.errorBody().toString())
-                }
-            }catch (e: java.lang.Exception){
-
-            }
-        }*/
     }
+
+    fun showSnackBar(view: View){
+        //Snackbar(view)
+        val snackbar = Snackbar.make(view, "Replace with your own action",
+            Snackbar.LENGTH_LONG).setAction("Action", null)
+        snackbar.setActionTextColor(Color.BLUE)
+        val snackbarView = snackbar.view
+        snackbarView.setBackgroundColor(Color.LTGRAY)
+        val textView =
+            snackbarView.findViewById(android.support.design.R.id.snackbar_text) as TextView
+        textView.setTextColor(Color.BLUE)
+        textView.textSize = 28f
+        snackbar.show()
+    }
+
+    inner class Textwatcher(var v: View) : TextWatcher {
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+
+        override fun afterTextChanged(s: Editable?) {
+
+            when (v.getId()) {
+                R.id.editText_email -> if (!isValidEmail(editText_email.text.toString().trim())) {
+                    wrapper_email.error = "Invalid email format"
+                }else{
+                    wrapper_email.error = ""
+                }
+
+                R.id.editText_password -> if (!isValidPassword(editText_password.text.toString().trim())) {
+                    wrapper_password.error = "Password must have at least 6 characters with at least one Capital letter, at least one lower case letter and at least one number"
+                }else{
+                    wrapper_password.error = ""
+                }
+            }
+
+            if(isValidEmail(editText_email.text.toString().trim()) && isValidPassword(editText_password.text.toString().trim())){
+                button_login.setEnabled(true);
+            }else{
+                button_login.setEnabled(false);
+            }
+
+        }
+    }
+
+    private fun isValidEmail(email: String?): Boolean {
+         return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isValidPassword(password: String?) : Boolean {
+        val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,}$"
+        val passwordMatcher = Regex(passwordPattern)
+
+        return (passwordMatcher.find(password) != null) true
+        else
+           false
+    }
+/*
+    fun isValidPassword(password: String?) : Boolean {
+        password?.let {
+            val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,}$"
+            val passwordMatcher = Regex(passwordPattern)
+            return (passwordMatcher.find(password) != null)
+        } ?: return false
+    }*/
+
 }
 
 /*
